48ef199e99540a5a7fb4bd24a2b3b872cc10f9ed
Deakristina <deakristina@gmail.com>
2018-12-13 07:28:33 +0100

    navBar and local

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css'; //NO
-// import PostJob from './postJob';
+import PostJob from './components/PostJob';
 import MainNavBar from './MainNavBar'; //TESTING ZONE
 import Register from './Register'; //UNDER CONSTRUCTION
 // import SearchJob from './SearchJob' //NOT CONSTRUCTED
 import Login from './Login'; //NOT CONSTRUCTED
 import ProfilePage from './ProfilePage'; //UNDER CONSTRUCTION
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
 	constructor() {
@@ -22,11 +23,13 @@ class App extends Component {
 
 	render() {
 		var router = {
+			//FindYourJob: <FindYourJob changePage={this.changeCurrentPage} />,
 			// searchJob: <SearchJob/>,
 			register: <Register changePage={this.changeCurrentPage} />,
 			login: <Login changePage={this.changeCurrentPage} />,
-			profile: <ProfilePage />
-			//Here the component that renders post job
+
+			profile: <ProfilePage />,
+			postJob: <PostJob changePage={this.changeCurrentPage} />
 		};
 		return (
 			<div>
