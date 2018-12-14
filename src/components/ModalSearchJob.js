a8bb6d0db6d2ee893d5501747e5e4272c20bb32f
Deakristina <deakristina@gmail.com>
2018-12-14 17:07:14 +0100

    14 dec

---
+++ src/components/ModalSearchJob.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
+
+class SearchResultModal extends Component {
+	state = {
+		data: {
+			title: '',
+			startDate: '',
+			duration: '',
+			address: '',
+			zipCode: '',
+			city: '',
+			country: '',
+			salary: '',
+			description: '',
+			requirements: []
+		}
+	};
+	toggle = () => {
+		this.setState({
+			modal: !this.state.modal
+		});
+	};
+
+	render() {
+		// let data = this.state.data.map((item, i) => {
+		// 	return <li>{item.info.title}</li>;
+		// });
+
+		return (
+			<div>
+				<Modal isOpen={this.props.modal} toggle={this.props.toggleModal} className={this.props.className}>
+					<ModalBody className="text-center">check!</ModalBody>
+					<ModalFooter>
+						<Button color="primary" onClick={this.props.toggleModal}>
+							Save
+						</Button>{' '}
+						<Button color="secondary" onClick={this.props.toggleModal}>
+							Back to Search
+						</Button>
+					</ModalFooter>
+				</Modal>
+			</div>
+		);
+	}
+}
+
+export default SearchResultModal;
--- src/components/SearchJob.js
+++ src/components/SearchJob.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import Input from './input';
 import axios from 'axios';
 import local from '../local';
+import Background from './img/course06.jpg';
+import ModalSearchResult from './ModalSearchJob';
+
+let searchJobImage = {
+	width: '100%',
+	height: '100vh',
+	backgroundImage: `url(${Background})`,
+	backgroundRepeat: 'no-repeat',
+	backgroundSize: 'cover',
+	opacity: 0.5
+};
 
 class SearchJob extends Component {
 	constructor(props) {
@@ -13,9 +24,14 @@ class SearchJob extends Component {
 			},
 			jobs: [],
 			search: '',
-			noResult: false
+			noResult: false,
+			showModal: false
 		};
 	}
+
+	toggleModal = () => {
+		this.setState({ showModal: !this.state.showModal });
+	};
 
 	handleChange = ({ currentTarget: input }) => {
 		const data = { ...this.state.data };
@@ -40,16 +56,31 @@ class SearchJob extends Component {
 
 	render() {
 		const { data } = this.state;
-		console.log(this.state.jobs, this.state.search);
 		let jobs;
 		if (this.state.jobs.length > 0) {
 			jobs = this.state.jobs.map((job, i) => {
 				return (
-					<ul key={i}>
-						<li>{job.info.title}</li>
-						<li>{job.location.address}</li>
-						<li>{job.info.salary}</li>
-					</ul>
+					<div className="job-result d-flex justify-content-between">
+						<div>
+							<ul className=" p-2" key={i}>
+								<li className="job-list">
+									<strong>Job Position: </strong> {job.info.title}
+								</li>
+								<li className="job-list">
+									<strong> Job Location: </strong> {job.location.city}, {job.location.country}
+								</li>
+								<li className="job-list">
+									<strong> Salary: </strong> {job.info.salary}
+								</li>
+							</ul>
+						</div>
+						<div className="">
+							<ModalSearchResult modal={this.state.showModal} toggleModal={this.toggleModal} />
+							<button className="btn btn-info mt-2 mr-2" onClick={this.toggleModal}>
+								Show Details
+							</button>
+						</div>
+					</div>
 				);
 			});
 		} else if (this.state.jobs.length === 0 && data.title) {
@@ -58,37 +89,44 @@ class SearchJob extends Component {
 			jobs = <React.Fragment />;
 		}
 		return (
-			<div className="container">
-				<h3>Find jobs around you</h3>
-				<form onSubmit={this.handleSubmit}>
-					<div className="form-row mt-5">
-						<div className="col-md-6">
-							<Input
-								name="title"
-								value={data.title}
-								onChange={this.handleChange}
-								type="text"
-								placeholder="Search by job title"
-							/>
-						</div>
+			<div id="home" className="hero-area">
+				<div className="bg-image bg-parallax overlay" style={searchJobImage} />
+				<div className="home-search-wrapper">
+					<div className="container">
+						<h1 className="search-title text-center">Find jobs around you</h1>
+						<form onSubmit={this.handleSubmit}>
+							<div className="form-row ">
+								<div className="col-md-6">
+									<Input
+										name="title"
+										label="What position are you looking for?"
+										value={data.title}
+										onChange={this.handleChange}
+										type="text"
+										placeholder="Search by position"
+									/>
+								</div>
 
-						<div className="col-md-6">
-							<Input
-								name="location"
-								value={data.location}
-								onChange={this.handleChange}
-								type="text"
-								placeholder="Search by city or country"
-							/>
-						</div>
+								<div className="col-md-6">
+									<Input
+										name="location"
+										label="Where do you want to work?"
+										value={data.location}
+										onChange={this.handleChange}
+										type="text"
+										placeholder="Search by city or country"
+									/>
+								</div>
+							</div>
+							<div className="row mt-4">
+								<div className="col-12 text-center">
+									<button className="btn btn-warning btn-lg">Search</button>
+								</div>
+							</div>
+						</form>
+						<div className="job-result-container">{jobs}</div>
 					</div>
-					<div className="row mt-4">
-						<div className="col-12 text-center">
-							<button className="btn btn-primary btn-lg">Search</button>
-						</div>
-					</div>
-				</form>
-				<div>{jobs}</div>
+				</div>
 			</div>
 		);
 	}
